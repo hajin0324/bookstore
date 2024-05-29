35e99813d468219b96ee184493015599eac534a6
hajin0324 <hajinn24@gmail.com>
2024-05-29 20:15:00 +0900

    feat: 전역 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Layout from "./components/layout/Layout";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
+import { GlobalStyle } from "./style/global";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <>
+      <GlobalStyle />
+      <Layout>
+        <Home />
+      </Layout>
+    </>
   );
 }
 
