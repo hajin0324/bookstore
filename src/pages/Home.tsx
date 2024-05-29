73425908d1bc7af05cf9fccc0a0e02a88bce3dcd
hajin0324 <hajinn24@gmail.com>
2024-05-29 20:06:56 +0900

    feat: layout 컴포넌트 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,6 @@
-import Header from "../components/common/Header";
-import { formatNumber } from "../utils/format";
-
 function Home() {
   return (
     <>
-      <Header />
       <div>book store</div>
     </>
   )
