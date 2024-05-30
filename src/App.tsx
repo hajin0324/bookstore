60b98e03f91a023d3b203dbc88bc2305150b6707
hajin0324 <hajinn24@gmail.com>
2024-05-30 23:32:11 +0900

    feat: 회원가입 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,34 @@ import { useContext } from "react";
 import { BookStoreThemeProvider, ThemeContext } from "./context/themeContext";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Error from "./components/common/Error";
+import Signup from "./pages/Signup";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout><Home /></Layout>,
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
     errorElement: <Error />
   },
   {
     path: "/books",
-    element: <Layout><div>도서 목록</div></Layout>
+    element: (
+      <Layout>
+        <div>도서 목록</div>
+      </Layout>
+    )
   },
+  {
+    path: "/signup",
+    element: (
+    <Layout>
+      <Signup />
+    </Layout>
+    )
+  }
 ]);
 
 function App() {
