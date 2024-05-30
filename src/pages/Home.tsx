20ce8d31cdca933f5e8ac03e879cc2f9be4c7291
hajin0324 <hajinn24@gmail.com>
2024-05-30 11:57:26 +0900

    feat: Button 컴포넌트 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import Button from "../components/common/Button";
 import Title from "../components/common/Title";
 
 function Home() {
   return (
     <>
       <Title size="medium">title</Title>
+      <Button size="large" scheme="normal">button</Button>
       <div>book store</div>
     </>
   );
