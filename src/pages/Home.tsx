4144cc9fd733246412e2f7804927f854c8cd3c3f
hajin0324 <hajinn24@gmail.com>
2024-05-30 11:47:19 +0900

    feat: Title 컴포넌트 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
+import Title from "../components/common/Title";
+
 function Home() {
   return (
     <>
+      <Title size="medium">title</Title>
       <div>book store</div>
     </>
-  )
+  );
 };
 
 export default Home;
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,10 +1,16 @@
 export type ThemeName = "light" | "dark";
-type ColorKey = "primary" | "background" | "secondary" | "third";
+export type ColorKey = "primary" | "background" | "secondary" | "third";
+export type HeadingSize = "large" | "medium" | "small";
 
 interface Theme {
   name: string;
   color: Record<ColorKey, string>;
-}
+  heading: {
+    [key in HeadingSize]: {
+      fontSize: string;
+    };
+  };
+};
 
 export const light: Theme = {
   name: "light",
@@ -13,10 +19,22 @@ export const light: Theme = {
     background: "lightgray",
     secondary: "blue",
     third: "green"
+  },
+  heading: {
+    large: {
+      fontSize: "2rem"
+    },
+    medium: {
+      fontSize: "1.5rem"
+    },
+    small: {
+      fontSize: "1rem"
+    }
   }
 };
 
 export const dark: Theme = {
+  ...light,
   name: "dark",
   color: {
     primary: "coral",
