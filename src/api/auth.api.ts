c2a1ce3174413d3087e1f2f24d12739b3261b501
hajin0324 <hajinn24@gmail.com>
2024-05-31 15:19:03 +0900

    feat: 로그인 기능 구현 및 상태 저장

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,6 +1,10 @@
 import { SignupProps } from "../pages/Signup";
 import { httpClient } from "./http";
 
+interface LoginResponse {
+  token: string;
+};
+
 export const signup = async (userData: SignupProps) => {
   const response = await httpClient.post("/users/join", userData);
   return response.data;
@@ -13,5 +17,10 @@ export const resetRequest = async (data: SignupProps) => {
 
 export const resetPassword = async (data: SignupProps) => {
   const response = await httpClient.put("/users/reset_pwd", data);
+  return response.data;
+};
+
+export const login = async (data: SignupProps) => {
+  const response = await httpClient.post<LoginResponse>("/users/login", data);
   return response.data;
 };
