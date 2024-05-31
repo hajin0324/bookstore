405cd1d8efa0f1a540e971db779548ba97338cf0
hajin0324 <hajinn24@gmail.com>
2024-05-31 22:53:05 +0900

    feat: 도서 목록 페이지 구현

---
+++ src/components/books/BooksList.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import BookItem from "./BookItem";
+import { Book } from "../../models/book.model";
+
+interface Props {
+  books: Book[];
+};
+
+function BooksList({ books }: Props) {
+  return (
+    <BooksListStyle>
+      {
+        books?.map((item) => (
+          <BookItem key={item.id} book={item} />
+        ))
+      }
+    </BooksListStyle>
+  );
+};
+
+const BooksListStyle = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 24px;
+`;
+
+export default BooksList;
